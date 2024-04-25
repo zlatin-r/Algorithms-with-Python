74b03c3f5f36c3f80d92e41578e9a95e6b878a48
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-25 09:11:03 +0300

    Solution to the task

---
+++ 03_Recursion Lab/07_Recursive_Fibonacci.py
@@ -0,0 +1,19 @@
+def calc_fib(number):
+    if number <= 1:
+        return 1
+    return calc_fib(number - 1) + calc_fib(number - 2)
+
+
+def iterative_fib(number):
+    fib0 = 1
+    fib1 = 1
+    result = 0
+
+    for _ in range(number - 1):
+        result = fib0 + fib1
+        fib0, fib1 = fib1, result
+    return result
+
+
+print(calc_fib(10))
+print(iterative_fib(10))
