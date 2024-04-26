f1c9e3226779d1855e9ebeb5953f4673b3e0a51a
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-26 12:53:38 +0300

    Solution to the task

--- 03_Searching and Sorting Algorithms/01_Binary_Search.py
+++ 03_Searching and Sorting Algorithms/01_Binary_Search.py
@@ -1,16 +1,19 @@
-numbers = [int(x) for x in input().split()]
-target = int(input())
-
-
-def binary_search(numbers, target):
-    left, right = 0, len(numbers) - 1
+def binary_search(nums, trg):
+    left, right = 0, len(nums) - 1
     while left <= right:
         mid = (left + right) // 2
-        if numbers[mid] == target:
+
+        if nums[mid] == trg:
             return mid
 
-        if numbers[mid] < target:
+        if nums[mid] < trg:
             left = mid + 1
         else:
             right = mid - 1
-        return -1
+    return -1
+
+
+numbers = [int(x) for x in input().split()]
+target = int(input())
+
+print(binary_search(numbers, target))
