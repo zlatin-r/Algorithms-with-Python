01264495bff704071ba62be5ebdc6ca7b9950b57
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-04-26 13:09:39 +0300

    Solution to the task

---
+++ 03_Searching and Sorting Algorithms/03_Bubble_Sort_2.py
@@ -0,0 +1,13 @@
+nums = [int(x) for x in input().split()]
+is_sorted = False
+i = 0
+
+while not is_sorted:
+    is_sorted = True
+    for j in range(1, len(nums) - i):
+        if nums[j - 1] > nums[j]:
+            nums[j], nums[j - 1] = nums[j - 1], nums[j]
+            is_sorted = False
+    i += 1
+
+print(*nums, sep=" ")
