207856b33425219fb97bc8fd508672c7fc2841da
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-05-01 16:32:48 +0300

    Solution to the task

--- 04_Introduction to Graphs/DFS_2.py
+++ 04_Introduction to Graphs/DFS_2.py
@@ -1,28 +1,14 @@
-def dfs(node, graph, visited):
-    if node in visited:
-        return
+graph = [
+    [3, 6],
+    [3, 6, 4, 2, 5],
+    [1, 4, 5],
+    [5, 0, 1],
+    [1, 2, 6],
+    [2, 1, 3],
+    [0, 1, 4]
+]
 
-    visited.add(node)
-
-    for child in graph[node]:
-        dfs(child, graph, visited)
-
-    print(node, end=" ")
-
-
-graph = {
-    1: [19, 21, 14],
-    19: [7, 12, 31, 21],
-    7: [1],
-    12: [],
-    31: [21],
-    21: [14],
-    14: [6, 23],
-    23: [21],
-    6: []
-}
-
-visited = set()
+visited = [False] * len(graph)
 
 for node in graph:
     dfs(node, graph, visited)
