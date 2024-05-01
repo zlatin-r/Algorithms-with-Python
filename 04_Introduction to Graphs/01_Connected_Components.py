fa752d4a2edf2ce153bcbe5e5e9b93c5bd84dca3
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-05-01 16:55:57 +0300

    Solution to the task

--- 04_Introduction to Graphs/01_Connected_Components.py
+++ 04_Introduction to Graphs/01_Connected_Components.py
@@ -1,7 +1,27 @@
+def dfs(node, graph, visited, component):
+    if visited[node]:
+        return
+    visited[node] = True
+
+    for child in graph[node]:
+        dfs(child, graph, visited, component)
+
+    component.append(node)
+
+
 nodes = int(input())
+visited = [False] * nodes
 graph = []
 
 for node in range(nodes):
     line = input()
     children = [] if line == '' else [int(x) for x in line.split()]
     graph.append(children)
+
+for node in range(nodes):
+    if visited[node]:
+        continue
+
+    component = []
+    dfs(node, graph, visited, component)
+    print(component)
