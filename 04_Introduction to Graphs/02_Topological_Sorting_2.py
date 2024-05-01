d31ec80fb60ff35445265eab017e9918a56e8deb
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-05-01 17:19:39 +0300

    Solution to the task

--- 04_Introduction to Graphs/02_Topological_Sorting_2.py
+++ 04_Introduction to Graphs/02_Topological_Sorting_2.py
@@ -1,25 +1,27 @@
-def find_node_without_dependencies(dependencies_by_node):
-    for node, dependencies in dependencies_by_node.items():
-        if dependencies == 0:
-            return node
-    return None
+from collections import deque
 
 
-def find_dependencies(graph):
-    result = {}
-    for node, children in graph.items():
-        if node not in result:
-            result[node] = 0
-        for child in children:
-            if child not in result:
-                result[child] = 1
-            else:
-                result[child] += 1
-    return result
+def dfs(node, graph, visited, cycles, sorted_nodes):
+    if node in cycles:
+        raise Exception
+    if node in visited:
+        return
+
+    visited.add(node)
+    cycles.add(node)
+
+    for child in graph[node]:
+        dfs(child, graph, visited, cycles, sorted_nodes)
+
+    cycles.remove(node)
+    sorted_nodes.appendleft(node)
 
 
 nodes = int(input())
 graph = {}
+
+visited = set()
+cycles = set()
 
 for _ in range(nodes):
     line_parts = input().split("->")
@@ -27,23 +29,9 @@ for _ in range(nodes):
     children = line_parts[1].strip().split(", ") if line_parts[1] else []
     graph[node] = children
 
-dependencies_by_node = find_dependencies(graph)
-has_cycles = False
-sorted_nodes = []
+sorted_nodes = deque()
 
-while dependencies_by_node:
-    node_to_remove = find_node_without_dependencies(dependencies_by_node)
-    if node_to_remove is None:
-        has_cycles = True
-        break
+for node in graph:
+    dfs(node, graph, visited, cycles, sorted_nodes)
 
-    dependencies_by_node.pop(node_to_remove)
-    sorted_nodes.append(node_to_remove)
-
-    for child in graph[node_to_remove]:
-        dependencies_by_node[child] -= 1
-
-if has_cycles:
-    print("Invalid topological sorting")
-else:
-    print(f"Topological sorting: {', '.join(sorted_nodes)}")
+print(*sorted_nodes, sep=" ")
