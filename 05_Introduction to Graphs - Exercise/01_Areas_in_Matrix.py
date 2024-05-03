bdfb33528e4a7dbe29d1537ee9e0cc47def5f1d2
Zlatin Rusev <75085055+zlatin-r@users.noreply.github.com>
2024-05-03 11:12:15 +0300

    Solution to the task

--- 05_Introduction to Graphs - Exercise/01_Areas_in_Matrix.py
+++ 05_Introduction to Graphs - Exercise/01_Areas_in_Matrix.py
@@ -1,9 +1,25 @@
+def dfs(parent, row, col, matrix, visited):
+    if visited[row][col]:
+        return
+    if matrix[row][col] != parent:
+        return
+    if row < 0 or col < 0 or row >= len(matrix) or col >= len(matrix[0]):
+        return
+
+
+
+visited = []
 rows = int(input())
 cols = int(input())
 matrix = []
-visited = []
 
 for _ in range(rows):
     matrix.append(list(input()))
     visited.append([False] * cols)
 
+for row in range(rows):
+    for col in range(cols):
+        if visited[row][col]:
+            continue
+        key = matrix[row][col]
+        dfs(key, row, col, matrix, visited)
